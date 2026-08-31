c3353dcfba95e20c3b5a0afd36dba69e310e8a3f
prasithdevanan <prasith4545@gmail.com>
2026-08-31 16:04:43 +0530

    update

--- router/users.ts
+++ router/users.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('users route');
+    res.status(200).json({ message: 'Hello from the patient route', success: true });
+});
+
+router.get('/all', (req, res) => {
+    res.status(200).json({ message: 'Hello from the patient route', success: true });
 });
 
 export default router;
